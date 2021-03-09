6ecff0b7d561599e3ba0c818718a5cafbdf6f785
Umaima Eftikhar <umibondre@gmail.com>
2021-03-09 20:58:28 +0300

    Add project files.

---
+++ FSQA/FSQA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FSQA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
